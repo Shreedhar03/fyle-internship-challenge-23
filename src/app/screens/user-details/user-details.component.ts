f51e9c3db6ba3e4ff97e69284b748a06fcc04ba1
Shreedhar <urawane03@gmail.com>
2024-02-09 21:07:56 +0530

    fetch data from github api

--- src/app/screens/user-details/user-details.component.ts
+++ src/app/screens/user-details/user-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { Repository } from 'src/models/repository.model';
+import { UserData } from 'src/models/userdata.model';
 
 @Component({
   selector: 'app-user-details',
@@ -7,5 +11,49 @@ import { Component } from '@angular/core';
 })
 export class UserDetailsComponent {
   userImage: string = '../../../assets/user.jpg';
-  repositories: any[] = [1,2,3,4,5,6,7,8,9,10];
+  userDetails: UserData | undefined;
+  repositories: Repository[] =[];
+  username: string = '';
+
+  constructor(private router: ActivatedRoute, private navigate: Router, private apiService: ApiService) { }
+
+
+  ngOnInit(): void {
+    this.router.params.subscribe((params) => {
+      this.username = params['username'];
+      this.fetchUserDetails();
+      this.fetchUserRepos();
+    })
+  }
+
+  // Fetch user details
+  fetchUserDetails() {
+    this.apiService.getUser(this.username).subscribe({
+      next: (data) => {
+        this.userDetails = data;
+        console.log("userData", this.userDetails)
+      },
+      error: (error) => {
+        console.error(error.message);
+        if (error.message.includes('404')) {
+          this.navigate.navigate(['/not-found']);
+        }
+        this.repositories = [];
+      }
+    });
+  }
+
+  // Fetch user repositories
+  fetchUserRepos() {
+    this.apiService.getRepos(this.username).subscribe({
+      next: (repos:Repository[]) => {
+        this.repositories = repos;
+        console.log("repos", this.repositories);
+      },
+      error: (error) => {
+        console.error(error.message);
+        this.repositories = [];
+      }
+    });
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap, throwError } from 'rxjs';
+import { Repository } from 'src/models/repository.model';
+import { UserData } from 'src/models/userdata.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +14,11 @@ export class ApiService {
   ) { }
 
   getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+    return this.httpClient.get<UserData>(`https://api.github.com/users/${githubUsername}`);
   }
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername: string) {
+    return this.httpClient.get<Repository[]>(`https://api.github.com/users/${githubUsername}/repos`);
+  }
 }
